1bf27cc2cd6f8c4f7769821574d483d27a4c0ad7
William Dorval <will.rome.dorval@gmail.com>
2020-07-13 17:53:06 -0400

    created fromCML method for shipMaker

--- src/com/aredowl/ship/shipMaker.java
+++ src/com/aredowl/ship/shipMaker.java
@@ -7,6 +7,8 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.ParserConfigurationException;
 
+import com.aredowl.blocks.block;
+import com.aredowl.blocks.blockMaker;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
@@ -28,17 +30,20 @@ public abstract class shipMaker {
     }
 
     static public ship fromXML(String path) throws IOException, SAXException {
+        ship ship = new ship();
         File inFile = new File(path);
         Document doc = dBuilder.parse(inFile);
         NodeList nList = doc.getElementsByTagName("item");
+        block current;
         for(int i=0; i < nList.getLength(); i++){
             Node item = nList.item(i);
             Element block = (Element) item;
             logger.log(Level.INFO,"Block Element" + block.getAttribute("index"));
             Element part = (Element) block.getElementsByTagName("block").item(0);
             logger.log(Level.INFO,"Part Element" + part.getAttribute("index"));
+            current = blockMaker.fromXML(part);
+            ship.addBlock(current, Integer.parseInt(block.getAttribute("index")), Integer.parseInt(block.getAttribute("parent")));
         }
-        ship ship = new ship();
         return ship;
     }
 }
